6c959ea6236551df5e35bf3c703c5fe4182fbe2a
Rashik Wasik <rashikrajin@gmail.com>
2021-06-23 00:19:07 +0600

    Update 57. Remove characters in string except alphabets.c

--- Department of FET_CSE 204F(SUST)/Lab Assignment #02/Task #02(C Examples-Programiz)/57. Remove characters in string except alphabets.c
+++ Department of FET_CSE 204F(SUST)/Lab Assignment #02/Task #02(C Examples-Programiz)/57. Remove characters in string except alphabets.c
@@ -8,18 +8,21 @@ int main()
     printf("Enter a string: ");
     gets(str);
 
-    n = strlen(str);
+    //n = strlen(a);
+    for (n = 0; str[n] != '\0'; n = n+1);
 
-    printf("Output string: ");
+    printf("\nOutput string: ");
 
-    for(i=0; i<n; i=i+1)
+    for(i = 0; i < n; i = i+1)
     {
-        if ((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
-            printf("%c",str[i]);
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+            printf("%c", str[i]);
 
         else
             continue;
     }
+
+    printf("\n");
 
     return 0;
 }
