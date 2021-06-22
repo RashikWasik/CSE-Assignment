45835acebdc750f6f0df90211ab38e21445bc905
Rashik Wasik <rashikrajin@gmail.com>
2021-06-22 23:55:17 +0600

    Update 60. Copy string without using strcpy().c

--- Department of FET_CSE 204F(SUST)/Lab Assignment #02/Task #02(C Examples-Programiz)/60. Copy string without using strcpy().c
+++ Department of FET_CSE 204F(SUST)/Lab Assignment #02/Task #02(C Examples-Programiz)/60. Copy string without using strcpy().c
@@ -4,15 +4,18 @@ int main()
     int i, n;
     char a[100], b[100];
 
-    printf("Enter string s1: ");
+    printf("Enter string s1 (Before copying): ");
     gets(a);
 
-    n = strlen(a);
+    //n = strlen(a);
+    for (n = 0; a[n] != '\0'; n = n+1);
 
     for(i=0; i<n; i=i+1)
         b[i] = a[i];
 
-    printf("\nString s2: %s\n", b);
+    b[i] = '\0';
+
+    printf("\nString s2 (After copying): %s\n", b);
 
     return 0;
 }
